6239f5318ae35a294686da5b3bc057f1dc30afd7
w568w <1278297578@qq.com>
2022-06-29 18:50:16 +0800

    task3: fix wrongly implemented model

--- task3/main.py
+++ task3/main.py
@@ -16,17 +16,18 @@ class ESIM(nn.Module):
         self.word_num = word_num
         vector_size = words_dict.shape[1]
         self.embedding = nn.Embedding.from_pretrained(words_dict)
-        self.input_encoding = nn.ModuleList(
-            [nn.LSTM(vector_size, lstm_hidden_size, batch_first=True, bidirectional=True),
-             nn.LSTM(vector_size, lstm_hidden_size, batch_first=True, bidirectional=True)])
-        self.composition = nn.ModuleList(
-            [nn.LSTM(8 * lstm_hidden_size, lstm_hidden_size, batch_first=True, bidirectional=True),
-             nn.LSTM(8 * lstm_hidden_size, lstm_hidden_size, batch_first=True, bidirectional=True)])
+        self.input_encoding = \
+            nn.LSTM(vector_size, lstm_hidden_size, batch_first=True, bidirectional=True)
+        self.composition = \
+            nn.LSTM(8 * lstm_hidden_size, lstm_hidden_size, batch_first=True, bidirectional=True)
 
         self.mlp = nn.Sequential(
             nn.Dropout(),
+            nn.BatchNorm1d(lstm_hidden_size * 8),
             nn.Linear(lstm_hidden_size * 8, type_num),
             nn.Tanh(),
+            nn.BatchNorm1d(type_num),
+            nn.Linear(type_num, type_num),
             nn.Softmax(dim=-1)
         )
 
@@ -35,8 +36,8 @@ class ESIM(nn.Module):
         x = torch.flatten(x, 1, -1)  # (batch_size,2*word_num)
         x = self.embedding(x)  # (batch_size,2*word_num,vector_size)
         x = torch.chunk(x, 2, dim=1)  # A list of (batch_size,word_num,vector_size)
-        a_bar, _ = self.input_encoding[0](x[0])  # (batch_size,word_num,2*lstm_hidden_size)
-        b_bar, _ = self.input_encoding[1](x[1])  # (batch_size,word_num,2*lstm_hidden_size)
+        a_bar, _ = self.input_encoding(x[0])  # (batch_size,word_num,2*lstm_hidden_size)
+        b_bar, _ = self.input_encoding(x[1])  # (batch_size,word_num,2*lstm_hidden_size)
         E = torch.matmul(a_bar, torch.transpose(b_bar, 1, 2))  # (batch_size,word_num,word_num)
         a_tilde, b_tilde = torch.matmul(torch.softmax(E, dim=2), b_bar), \
                            torch.matmul(torch.transpose(torch.softmax(E, dim=1), 1, 2),
@@ -44,8 +45,8 @@ class ESIM(nn.Module):
         m_a, m_b = torch.cat((a_bar, a_tilde, a_bar - a_tilde, a_bar * a_tilde), dim=2), \
                    torch.cat((b_bar, b_tilde, b_bar - b_tilde, b_bar * b_tilde),
                              dim=2)  # (batch_size,word_num,8*lstm_hidden_size)
-        v_a, _ = self.composition[0](m_a)  # (batch_size,word_num,lstm_hidden_size*2)
-        v_b, _ = self.composition[1](m_b)  # (batch_size,word_num,lstm_hidden_size*2)
+        v_a, _ = self.composition(m_a)  # (batch_size,word_num,lstm_hidden_size*2)
+        v_b, _ = self.composition(m_b)  # (batch_size,word_num,lstm_hidden_size*2)
 
         kernel_size = (self.word_num, 1)
         v = torch.cat((F.avg_pool2d(v_a, kernel_size), F.max_pool2d(v_a, kernel_size), F.avg_pool2d(v_b, kernel_size),
@@ -92,15 +93,15 @@ def main():
                             typ='series')  # Series 保证 data 的结构是很多行 json，而不是一个表格（Frame）
     # 下载 Glove 变量
     glove = vocab.GloVe(name='6B', dim=300)
-    x, y = generate_vectors(glove, data[:1000])
+    x, y = generate_vectors(glove, data[:2000])
     x = torch.from_numpy(x).to(device)
     y = torch.tensor(y).to(device)
-    per = 800
-    batch_size = 10
+    per = 1600
+    batch_size = 32
 
-    model = ESIM(glove.vectors, x.shape[-1], 600, 3).to(device)
+    model = ESIM(glove.vectors, x.shape[-1], 300, 3).to(device)
     loss_fn = nn.CrossEntropyLoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=5e-5)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.0004)
 
     loss_history = []
     for epoch in range(100):
